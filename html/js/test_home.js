d799f66420db5ec8ee7dc90fa4c6b4084e1010a3
Christopher Lovejoy <lovejoy.chris@gmail.com>
2011-10-01 15:12:43 -0700

    adding test page to show Java simultaneously

---
+++ html/js/test_home.js
@@ -0,0 +1,44 @@
+function loadMixblendr() {
+	embedMixblendr( {
+		containerId: "mbapp",
+		id: "mixblendr",
+		code: "com/mixblendr/gui/main/Applet",
+		archive: "mixblendr/mixblendr.jar?TIMESTAMP="+(new Date()).getTime(),
+		width: 1,
+		height: 1,
+		params: {
+			url: "mixblendr/competition/getfile",
+			REDIRECT_URL: "mixblendr/competition/competition-entries/",
+			DEFAULT_TEMPO: "120.0"
+		}
+	} );
+};
+var flashvars = {
+	sampleListUrl:"scripts/samples.json.php",
+	countryListUrl:"js/country_codes.json",
+	saveUrl:"scripts/save_mix.php",
+	registerUrl:"scripts/register.php",
+	demoMixUrl:"audio/mixes/demo_mix_01.mp3"
+};
+var attributes = {
+	menu: "false"
+};
+swfobject.embedSWF(
+	"swf/main.swf",
+	"sfapp",
+	"100%",
+	"100%",
+	"9.0.0",
+	"swf/expressInstall.swf",
+	flashvars,
+	null,
+	attributes,
+	function(e){
+		e.success && mbinterface.addEventListenerObject(e.ref);
+	}
+);
+function alertAsync(msg) {
+	setTimeout(function(){
+		alert(msg);
+	},0);
+}
---
+++ html/testing.php
@@ -0,0 +1,21 @@
+<?php
+$includes = array('layout');
+$css = array('test_home');
+$js = array('swfobject', 'javaobject', 'mixblendr', 'test_home');
+require('inc/header.php');
+?>
+		<div id="app_container">
+			<div id="sfapp">
+				<h1>Stereofyter requires Adobe Flash.</h1>
+				<p>Please update your browser's Flash plugin by clicking the button below.</p>
+				<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
+			</div>
+		</div>
+		<div class="footer">
+			The Stereofyter is a project of On the Map Records (OtMR), a non-profit organization that uses music to fight stereotypes.<br />
+			OtMR is fiscally sponsored by Artspire, a project of the New York Foundation for the Arts (NYFA). Visit <a href="http://www.onthemaprecords.org" target="_blank">www.onthemaprecords.org</a> for more information. 
+		</div>
+		<div id="mbapp">
+			<!--<applet CODE="com/mixblendr/gui/main/Applet" ARCHIVE="mixblendr/mixblendr.jar" WIDTH="800" HEIGHT="600" ALT="Your browser is not configured to view the applet. Please install Jave Runtime JRE 1.5 or higher." id="mixblendr"><PARAM name="url" value="competition/getfile"><PARAM name="REDIRECT_URL" value="competition/competition-entries/"><PARAM name="DEFAULT_TEMPO" value="120.0"></applet>-->
+		</div>
+<?php require('inc/footer.php'); ?>
