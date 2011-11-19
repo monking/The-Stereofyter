e56606c3cbbddcb2031004af5beb4b240e22dd00
Christopher Lovejoy <c@chrislovejoy.com>
2011-11-19 10:35:11 -0800

    allowing multiple domains for local and remote DB config

--- _config.php
+++ _config.php
@@ -1,12 +1,12 @@
 <?php
 define('MIXER_APP_VERSION', '06003');
 define('MIXER_ENGINE_VERSION', '0102');
-if ($_SERVER['SERVER_NAME'] == 'local.stereofyter.org') {
+if (strpos($_SERVER['SERVER_NAME'], 'local') !== FALSE) {
   define('DB_HOST', '127.0.0.1');
   define('DB_USER', 'forum');
   define('DB_PASS', 'australia');
   define('DB_NAME', 'stereofyter');
-} elseif ($_SERVER['SERVER_NAME'] == 'stereofyter.chrislovejoy.com') {
+} elseif (strpos($_SERVER['SERVER_NAME'], 'chrislovejoy.com') !== FALSE) {
   define('DB_HOST', 'internal-db.s7816.gridserver.com');
   define('DB_USER', 'db7816_stereofy');
   define('DB_PASS', 'australia');
@@ -17,4 +17,5 @@ if ($_SERVER['SERVER_NAME'] == 'local.stereofyter.org') {
   define('DB_PASS', 'australia');
   define('DB_NAME', 'db85217_stereofyte_app');
 }
+
 ?>
